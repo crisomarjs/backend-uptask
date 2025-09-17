ab2575c0676fe4bbb9adeb539ad842bb3583ee86
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-09-17 13:29:45 -0600

    Se creo endpoints para agregar, listar y eliminar notas, se modifico el controlador y modelo de la tarea para poder ver las notas

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -7,6 +7,7 @@ import { projectExists } from "../middleware/project";
 import { hasAuthorization, taskBelongsToProjects, taskExists } from "../middleware/task";
 import { authenticate } from "../middleware/auth";
 import { TeamMemberController } from "../controllers/TeamController";
+import { NoteController } from "../controllers/NoteController";
 
 const router = Router()
 
@@ -124,6 +125,22 @@ router.delete('/:projectId/team/:userId',
         .isMongoId().withMessage('ID No válido'),
     handleInputErrors,
     TeamMemberController.removeMemberById
+)
+
+/** Routes for Notes */
+router.post('/:projectId/tasks/:taskId/notes',
+    body('content')
+        .notEmpty().withMessage('El Contenido de la nota es obligatorio'),
+    handleInputErrors,
+    NoteController.createNote
+)
+
+router.get('/:projectId/tasks/:taskId/notes',
+    NoteController.getTaskNotes
+)
+router.delete('/:projectId/tasks/:taskId/notes/:noteId',
+    param('noteId').isMongoId().withMessage('ID No Válido'),
+    NoteController.deleteNote
 )
 
 export default router
